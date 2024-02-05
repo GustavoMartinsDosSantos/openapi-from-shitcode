09dbe3083a6c3dbd0e54803288bba14b28f5735e
Gustavo Martins dos Santos <guga.m.s8@hotmail.com>
2024-02-05 07:32:49 -0300

    fix: removed async method since ts.forEachChild doesn't handle it well \nfeat: add fileUrl for types with same name

--- src/listFunctions.ts
+++ src/listFunctions.ts
@@ -1,8 +1,13 @@
 import ts from 'typescript';
 import fs from 'fs/promises'
 
+interface TypeObject {
+  name: string;
+  body: {};
+  fileUrl: string
+}
+
 export function extractExportsFromFile(fileName: string) {
-  // Lê o arquivo
   const sourceFile = ts.createSourceFile(
     fileName,
     ts.sys.readFile(fileName)!,
@@ -10,36 +15,46 @@ export function extractExportsFromFile(fileName: string) {
     true
   );
 
-  ts.forEachChild(sourceFile, nodeIterationCallback);
+  const sourceFileNodes: ts.Node[] = []
+  const wholeProjectTypes: TypeObject[] = [];
+
+ async function exec(){
+    ts.forEachChild(sourceFile, (node) => {
+      sourceFileNodes.push(node);
+    });
+    await Promise.all(sourceFileNodes.map(nodeIterationCallback))
+    console.log(wholeProjectTypes);
+  }
 
   async function nodeIterationCallback(node: ts.Node) {
-    console.log(node?.getText(), ts.isExportDeclaration(node));
-    // if (!node.getText()) return;
+    if (!node.getText()) return;
 
-    // if(ts.isImportDeclaration(node) || ts.isExportDeclaration(node)){
-    //   const importFilePath = node.getText().split('from')[1].replaceAll('"', '').replace(';', '');
-    //   const concatPath = `${fileName.split('/').slice(0, fileName.split('/').length - 1).join('/').trim()}/${importFilePath.trim()}`
-    //   wholeProjectInterfaces.push(...(await getAllInterfacesOnFile(concatPath)))
-    // }
-    nodeIterationCallback(node.getChildAt(2))
+    if(ts.isImportDeclaration(node) || ts.isExportDeclaration(node)){
+      const importFilePath = node.getText().split('from')[1].replaceAll('"', '').replace(';', '');
+      const concatPath = `${fileName.split('/').slice(0, fileName.split('/').length - 1).join('/').trim()}/${importFilePath.trim()}`
+      wholeProjectTypes.push(...(await getAllInterfacesOnFile(concatPath)))
+    }
   }
+  exec();
 }
 
 
 //UTILS
-async function getAllInterfacesOnFile (filePath: string, fileExtension = 'ts'): Promise<{name: string, body: {}}[]> {
-  const wholeFile = await fs.readFile(`${filePath}.${fileExtension}`, {encoding: 'utf8'})
+async function getAllInterfacesOnFile (filePath: string, fileExtension = 'ts'): Promise<TypeObject[]> {
+  const parsedFilePathWithFileExtension = `${filePath}.${fileExtension}`.replaceAll('/./', '/');
+  const wholeFile = await fs.readFile(parsedFilePathWithFileExtension, {encoding: 'utf8'})
   return Array.from(
     wholeFile.matchAll(/(export\s)*interface\s*[A-Z+]\w*\s*{.*\s[\s\w\d:;]*}/g))
     .map(elem => {
-      const splittedAndParsedInterface = elem[0].replace('export interface ', '').split('{', )
+      const splittedAndParsedInterface = elem[0].replace(/(export[\s]*)*(interface )/g, '').split('{', )
       splittedAndParsedInterface[1] = splittedAndParsedInterface[1].replaceAll(/[\n\r\s]*/g, '').replaceAll(';', ',').trim();
       splittedAndParsedInterface[1] = splittedAndParsedInterface[1].split(',').map(
         interfaceBody => interfaceBody.split(':').map(entryString => `"${entryString}"`).join(':')
         ).join(',').replace(',"}"', ' }');
       return {
         name: splittedAndParsedInterface[0].trim() as string,
-        body: JSON.parse(`{ ${splittedAndParsedInterface[1]}`) as {}
+        body: JSON.parse(`{ ${splittedAndParsedInterface[1]}`) as {},
+        fileUrl: parsedFilePathWithFileExtension
       }
     });
   
